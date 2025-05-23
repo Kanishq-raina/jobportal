0d95fcec237f36895752eac2acdeb6e8a26bfef7
KANISHQ RAINA <120960655+Kanishq-raina@users.noreply.github.com>
2025-05-24 00:29:23 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -52,7 +52,7 @@ const Home = () => {
 
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://jobportal-xqgm.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({identifier: username, password }),
