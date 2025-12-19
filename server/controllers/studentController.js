9f900157cd042931d8a2f01b9456bf1ad21e62d0
KANISHQ RAINA <120960655+Kanishq-raina@users.noreply.github.com>
2025-12-19 21:40:54 +0530

    Update studentController.js

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -10,10 +10,7 @@ import crypto from "crypto";
 import redis from "../utils/redisClient.js";
 import { sendOTPEmail } from "../utils/sendAdminOTPEmail.js";
 import { sendConfirmation } from "../utils/sendStudentEmailUpdateConfirmation.js";
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 
 
 
