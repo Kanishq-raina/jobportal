95e8c54feae288afc7e8aedf5e60679a210227a4
KANISHQ RAINA <120960655+Kanishq-raina@users.noreply.github.com>
2025-12-19 20:35:26 +0530

    Update redisClient.js

--- server/utils/redisClient.js
+++ server/utils/redisClient.js
@@ -1,10 +1,13 @@
-import { createClient } from 'redis';
+import { Redis } from '@upstash/redis'
 import dotenv from 'dotenv';
 dotenv.config();
 
 const redisClient = createClient({
   url: process.env.REDIS_URL ,
 });
+const redis = new Redis({
+ url: process.env.REDIS_URL ,
+})
 
 
 redisClient.on('error', (err) => console.error('❌ Redis Error:', err));
